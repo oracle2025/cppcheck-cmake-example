091a3fe7dafd68e301c546f949f3d69415f5dd4e
Richard Spindler <richard.spindler@gmail.com>
2018-07-21 21:59:40 +0200

    add copy paste checker target

---
+++ cmake/cpd.cmake
@@ -0,0 +1,17 @@
+#pmd cpd --minimum-tokens 100 --files main.cpp
+file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
+    string(FIND ${SOURCE_FILE} ${PROJECT_BINARY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
+    if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
+        list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
+    endif ()
+endforeach ()
+find_program(PMD pmd)
+add_custom_target(
+    copypaste
+    COMMAND ${PMD}
+    cpd
+    --minimum-tokens 100
+    --files ${ALL_SOURCE_FILES}
+    )
+
