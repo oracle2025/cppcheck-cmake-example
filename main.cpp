bb286a525f9e56c1f25f0f60e4bbb23641b1d1b7
Richard Spindler <richard.spindler@gmail.com>
2018-07-22 13:03:50 +0200

    adding leaking main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // Copyright 2018 Richard Spindler
 int main(int argc, const char *argv[]) {
-    // char *var = new char[5];
+    char *var = new char[5];
     char a[10];
     a[10] = 0;
     return 0;
